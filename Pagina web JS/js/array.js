22d32db97a8c6ac530507f55805677b7f566ab1c
Guillermo Villacura <g.villacurat@gmail.com>
2023-04-21 22:21:06 -0400

    Clase 21-04-2023

--- Pagina web JS/js/array.js
+++ Pagina web JS/js/array.js
@@ -34,3 +34,45 @@ for (const i of arrayDos) {
 }
 
 console.log(arrayDos);
+
+function region(){
+    const select = document.getElementById("cmbRegion");
+
+    for (const i of arrayDos) {
+        const option = document.createElement("option");
+        option.value = i.id;
+        option.textContent = i.nombre;
+        select.appendChild(option);
+    }
+}
+
+
+function valor(){
+    let valor = document.getElementById("cmbRegion").value;
+    console.log("valor: ",valor);
+}
+
+function load(){
+    region();
+    api();
+}
+
+
+function api(){
+    fetch('https://mindicador.cl/api')
+        .then(response => response.json())
+        .then(data =>{
+            console.log(data);
+            let dolar = document.getElementById("dolar");
+            dolar.innerHTML += data.dolar.valor;
+
+            let bitcoin = document.getElementById("bitcoin");
+            bitcoin.innerHTML += data.bitcoin.valor;
+
+            let euro = document.getElementById("euro");
+            euro.innerHTML += data.euro.valor;
+
+            let uf = document.getElementById("uf");
+            uf.innerHTML += data.uf.valor;
+        })
+}
