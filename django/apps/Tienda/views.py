b488a346e58d162c4a7aa6aa00462e23937e35ec
Guillermo Villacura <g.villacurat@gmail.com>
2023-06-08 20:40:26 -0400

    Clase 08-06-2023

--- django/apps/Tienda/views.py
+++ django/apps/Tienda/views.py
@@ -5,4 +5,12 @@ from .models import *
 
 def cargarInicio(request):
     productos = Producto.objects.all()
-    return render(request,"inicio.html",{"producto":productos})
+    cate_producto_jardineria = Producto.objects.filter(categoriaId = 1) 
+    cate_producto_arbol = Producto.objects.filter(categoriaId = 2) 
+    return render(request,"inicio.html",{"producto":productos,"cate_arbol":cate_producto_arbol,"cate_jardineria": cate_producto_jardineria})
+
+
+
+def cargarAgregarProducto(request):
+    categorias = Categoria.objects.all()
+    return render(request,"agregarProductos.html",{"cate":categorias})
